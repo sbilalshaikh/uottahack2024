b598fce0ea8a70600b4d021df23d095f38a9f8d4
Methira Herath <methira.19@gmail.com>
2024-03-03 01:19:34 -0500

    updated to load async

--- client/src/pages/GreaterMap.jsx
+++ client/src/pages/GreaterMap.jsx
@@ -1,109 +1,77 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { GoogleMap, LoadScript } from '@react-google-maps/api'
-import { fill } from 'lodash';
-
-const bounds = { 
-    
-    width : '50%',
-    height : '400px',
-    margin : '2.5% auto 0',
-    border: '3px solid #000000'
-
-}
-
-const center = {
-
-    lat: 45.340186, 
-    lng:-75.726175
-
-}
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { Loader } from "@googlemaps/js-api-loader";
 
 const response = await fetch("http://localhost:8000/neighbourhood/get-all");
 
 const GEO_JSON_DATA = await response.json();
 
-const neighborhoods = GEO_JSON_DATA.neighbourhoods;
+const bounds = {
+    width: '50%',
+    height: '400px',
+    margin: '2.5% auto 0',
+    border: '3px solid #000000'
+}
+
+const center = {
+    lat: 45.340186,
+    lng: -75.726175
+}
 
 const GreaterMap = () => {
-
     const navigate = useNavigate();
-
     const mapRef = useRef(null);
 
-    const onLoad = (map) => {
-
-        mapRef.current = map;
-        neighborhoods.forEach(element => {
-            const feature = map.data.addGeoJson(element.geoJSON);
-            const fillColor = element.color;
-            map.data.setStyle(function(feature) {
-                return {
-                  fillColor: fillColor,
-                  strokeWeight: 2,
-                  strokeColor: "black",
-                  fillOpacity: 0.5
-                };
-            });
+    useEffect(() => {
+        const loader = new Loader({
+            apiKey: 'AIzaSyCygEHJWZdXCELGXxk9k7iarJMVJ2JWVU0',
+            version: 'weekly', // Change this to your desired version
         });
 
-        // map.data.addGeoJson(KanataNorthGeoJSON);
-        // map.data.addGeoJson(SandyHillGeoJSON);
-        // map.data.addGeoJson(CarlingwoodGeoJSON);
+        loader.load().then(() => {
+            const map = new window.google.maps.Map(mapRef.current, {
+                center: center,
+                zoom: 10.9,
+                mapId: 'uOttaHacks6', // Change this to your desired map ID
+            });
 
-        map.data.addListener('click' , (event) => {
+            // Load your neighborhoods here using fetch and addGeoJson
+            const neighborhoods = GEO_JSON_DATA.neighbourhoods;
+            // Example:
+            neighborhoods.forEach(element => {
+                const feature = map.data.addGeoJson(element.geoJSON);
+                const fillColor = element.color;
+                map.data.setStyle(function(feature) {
+                    return {
+                        fillColor: fillColor,
+                        strokeWeight: 2,
+                        strokeColor: "black",
+                        fillOpacity: 0.8
+                    };
+                });
+            });
 
-            name = event.feature.getProperty('name');
-            console.log(name);
-            navigate(`/community/${name}`);
-
-        })
-
-    };
-
+            map.data.addListener('click', (event) => {
+                const name = event.feature.getProperty('name');
+                console.log(name);
+                navigate(`/community/${name}`);
+            });
+        });
+    }, [navigate]);
 
     const mapStyle = {
-
         width: '65%',
         height: '300px',
-        margin: '0 auto', 
+        margin: '0 auto',
         padding: '20px',
-
     }
 
-
-
-return (
-
-    <div>
-
-        <LoadScript
-            async googleMapsApiKey="AIzaSyCygEHJWZdXCELGXxk9k7iarJMVJ2JWVU0" 
-        >
-
-            <GoogleMap 
-            mapContainerStyle={bounds}
-            center={center}
-            zoom={10.9}
-            onLoad={onLoad}
-            style={mapStyle}
-
-            >
-            </GoogleMap>
-
-
-        </LoadScript>
-
-
-
-    </div>
-
-
-
+    return (
+        <div>
+            <div ref={mapRef} style={bounds}></div>
+        </div>
     );
-
-
 };
-
 
 export default GreaterMap;
