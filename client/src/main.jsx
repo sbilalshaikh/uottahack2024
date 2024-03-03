7cda9326ac6e1ad20a0c5c3064cda67ea850ce56
HasithDeAlwis <hasithde24@gmail.com>
2024-03-02 20:13:58 -0500

    setup react router dom

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,10 +5,22 @@ import './index.css'
 import AuthPage from './pages/AuthPage.jsx'
 import Navbar from './components/base/Navbar.jsx'
 import { ChakraProvider } from '@chakra-ui/react'
+import { Routes, Route } from "react-router-dom"
+
+import { BrowserRouter } from "react-router-dom";
+
+import Root from "./routes/root";
+import Community from './pages/Community.jsx';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ChakraProvider>
+    <BrowserRouter>
     <Navbar />
-    <AuthPage />
+    <Routes>
+        <Route path="/" element={ <AuthPage/> } />
+        <Route path="/community" element={ <Community/> } />
+    </Routes>
+    </BrowserRouter>
   </ChakraProvider>,
 )
