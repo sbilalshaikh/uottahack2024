d381f8b802fcba50e7fee3f1d89bfe9299ab719d
Bilal Shaikh <bshai1@ocdsb.ca>
2024-03-03 04:38:04 -0500

    added an extra button to nav bar

--- client/src/components/base/Navbar.jsx
+++ client/src/components/base/Navbar.jsx
@@ -17,11 +17,8 @@ import {
   Spacer
 } from "@chakra-ui/react";
 import { HamburgerIcon } from '@chakra-ui/icons';
-
 import { Link as ChakraLink, Tooltip, useToast } from "@chakra-ui/react";
 import { Link } from 'react-router-dom';
-
-
 
 const Navbar = () => {
   const toast = useToast();
@@ -30,11 +27,11 @@ const Navbar = () => {
   const [profile, setProfile] = useState();
   const [isSmallerThan768] = useMediaQuery('(max-width: 768px)');
 
-
   return (
     <Box bg="#D2E8C1" position={{ sm: "sticky" }} top={0} zIndex={2}>
       <Flex justify={{ base: "center", sm: "space-between" }} alignItems={{sm:"end"}}>
         
+        {/* Left side of the Navbar */}
         <Box padding={2} display={"flex"}> 
           <Box
             pb={2}
@@ -43,21 +40,15 @@ const Navbar = () => {
             flexDir={{ base: "column", sm: "row" }}
             wrap={{ base: "nowrap", md: "wrap" }}
           >
-            <Box maxW="200px" maxH="200px">
-              
-            </Box>
+            <Box maxW="200px" maxH="200px"></Box>
 
             <Input
               placeholder="Search Neighbourhood"
-              borderRadius="md" // Adjust the border radius
-              borderColor="teal.500" // Adjust the border color
-              _focus={{
-                borderColor: "teal.700", // Adjust the border color on focus
-              }}
-              _hover={{
-                borderColor: "teal.600", // Adjust the border color on hover
-              }}
-              color={"white"}
+              borderRadius="md"
+              borderColor="teal.500"
+              _focus={{ borderColor: "teal.700" }}
+              _hover={{ borderColor: "teal.600" }}
+              color="white"
               onChange={(e) => setSearchTerm(e.target.value)}
               value={searchTerm}
               marginTop={{ sm: "30px" }}
@@ -69,32 +60,28 @@ const Navbar = () => {
                 marginTop={{ base: "10px", sm: "30px" }}
                 flexBasis={{ base: "90%" }}
                 colorScheme="green"
-                justifySelf={"start"}
+                justifySelf="start"
                 marginLeft={{ base: 5, sm: 0 }}
               >
                 Search!
               </Button>
             </Box>
 
-
-            
-
-
           </Box>
         </Box>
 
-        <Box padding={{base: 2, sm: 4}}
-        justifySelf={"end"}>
-        {isSmallerThan768 ? (
+        {/* Right side of the Navbar */}
+        <Box padding={{ base: 2, sm: 4 }} justifySelf="end">
+          {isSmallerThan768 ? (
             <Menu>
               {({ isOpen }) => (
                 <>
-                <MenuButton
+                  <MenuButton
                     as={IconButton}
                     boxSize={{ base: '2.5em', sm: '3em' }}
                     icon={<HamburgerIcon />}
                     aria-label="Open menu" 
-                    />
+                  />
                   <MenuList>
                     <MenuItem>Maps</MenuItem>
                     <MenuItem>Community</MenuItem>
@@ -102,35 +89,38 @@ const Navbar = () => {
                 </>
               )}
             </Menu>
-        ) : (
-            // Render your box content for larger screens
-            <Box display="flex" alignItems="center"
-
-              marginTop={{ base: "5px", sm: "30px" }}
-              flexBasis={{ base: "90%" }}
-              colorScheme="green"
-              justifySelf={"start"}
-              marginLeft={{ base: 5, sm: 0 }}
-              padding={'0.5rem 1rem'}
-              color={'white'}
-              background={'#22a363'}
-              borderRadius={'0.3rem'}
-              fontWeight={'medium'}
-
-           >
-              <Spacer />
-              <Link to={"/map"} >
-                Main Map
-              </Link>
-            </Box>
-            
-        )}
-
-
-
-        
+          ) : (
+            <Flex alignItems="center">
+              <Box
+                marginTop={{ base: "5px", sm: "30px" }}
+                colorScheme={"green"}
+                justifySelf={"start"}
+                padding={'0.5rem 1rem'}
+                color={"white"}
+                background={"#22a363"}
+                borderRadius={"0.3rem"}
+                fontWeight={"medium"}
+                marginRight={2}
+              >
+                <Link to="/map">Main Map</Link>
+              </Box>
+              
+              <Box
+                marginTop={{ base: "5px", sm: "30px" }}
+                colorScheme={"green"}
+                justifySelf={"end"}
+                padding={'0.5rem 1rem'}
+                color={"white"}
+                background={"#22a363"}
+                borderRadius={"0.3rem"}
+                fontWeight={"medium"}
+              >
+                <Link to="/uploadPhoto">Upload File</Link>
+              </Box>
+            </Flex>
+          )}
         </Box>
-            
+        
       </Flex>
     </Box>
   );
